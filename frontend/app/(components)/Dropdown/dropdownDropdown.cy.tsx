fbdcc21673d054ba439125637dc2dba2d11a00ea
Dom Grad <Dominikgrad47@gmail.com>
2022-12-26 20:22:40 +0000

    remove tests

--- frontend/app/(components)/Dropdown/dropdownDropdown.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Dropdown from './dropdown'
-
-describe('<Dropdown />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Dropdown />)
-  })
-})
